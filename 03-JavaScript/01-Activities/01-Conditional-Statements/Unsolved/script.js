3432c104c3aec355f12e595959a355289a3db7f8
jrwj97 <jrwj97@aol.com>
2022-01-04 21:03:40 -0500

    classwork

--- 03-JavaScript/01-Activities/01-Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/01-Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,19 @@
 // TODO: Change the values and operators below to test your algorithm meets all conditions
-var x = 50;
+var x = 10;
 var expression1 = x < 25;
 var expression2 = x > 50;
 
 // TODO: Write Your JavaScript Code Here
+if (expression1 && expression2) {
+    console.log ("True ✅ True ✅")
+} else if (expression1) {
+    console.log ("True ✅ False ❌")
+} else if (expression2) {
+    console.log ("FALSE & TRUE")
+} else (
+    console.log ("FALSE & FALSE")
+)
+
+
+
+
--- 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
@@ -1,1 +1,8 @@
 // TODO: Write Your JavaScript Code Here
+var students = [ 'Sarah', 'Orlando', 'Heather', 'Ismael', 'Hung'];
+
+console.log(students.length);
+
+for (var i = 0; i < students.length; i++) {
+  console.log('Great to see you, ' + students[i] + '!');
+}   
